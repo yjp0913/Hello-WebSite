d5be729b142323ebc7798e4a4e5f72e5ed3d1058
YJ <yjp0913@gmail.com>
2021-09-12 12:39:06 +0900

    add files

---
+++ src/main/java/com/example/hellowebsite/HelloWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellowebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hellowebsite/HelloWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellowebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
